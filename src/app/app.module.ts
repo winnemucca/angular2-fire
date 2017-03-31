a32009b3b3752b9fb73ed7a0488e1f791ba002f1
Steven Kauyedauty <skauyedauty@gmail.com>
2017-03-31 00:37:52 -0600

    added flexbox playing with grid before I further work on firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { RouterModule } from '@angular/router';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+// third party
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home-component/home-component.component';
@@ -35,7 +40,9 @@ import { appRoutes } from './routes';
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule.forRoot(),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
